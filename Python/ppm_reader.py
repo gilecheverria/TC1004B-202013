166d63dfca9e4f367e199829fcc581e13eab7c3e
Gilberto Echeverria <g.echeverria@tec.mx>
2020-10-01 22:59:00 -0500

    Add Python example to work with PPM images

---
+++ Python/ppm_reader.py
@@ -0,0 +1,57 @@
+"""
+Open an manipulate PPM images
+    http://netpbm.sourceforge.net/doc/ppm.html
+
+Gil Echeverria
+25/09/2020
+"""
+
+
+def load_image(filename_in):
+    """ Open an image file and store its contents in a matrix """
+    image_data = []
+    with open(filename_in) as file:
+        # Read the image header
+        magic_number = file.readline().strip()
+        size = file.readline().split()
+        max_value = int(file.readline())
+        # Read the pixel data
+        for line in file:
+            row = line.split()
+            for i in range(len(row)):
+                row[i] = int(row[i])
+            image_data.append(row)
+    return image_data, max_value
+
+
+def image_negative(image_data, max_value):
+    """ Convert an image to its negative """
+    for r, row in enumerate(image_data):
+        for c, value in enumerate(row):
+            image_data[r][c] = max_value - value
+
+
+def write_image(image_data, max_value, filename_out):
+    """ Write the contents of the matrix into a new file """
+    with open(filename_out, "w") as file:
+        # Write the header for the PPM
+        file.write("P3\n")
+        file.write(f"{len(image_data[0]) // 3} {len(image_data)}\n")
+        file.write(f"{max_value}\n")
+        # Write the contents of the data matrix
+        for row in image_data:
+            for value in row:
+                file.write(f"{value:3} ")
+            file.write("\n")
+        
+
+def main():
+    """ Program starting point """
+    filename_in = "tester-a.ppm"
+    filename_out = "tester-a-n.ppm"
+    image_data, max_value = load_image(filename_in)
+    image_negative(image_data, max_value)
+    write_image(image_data, max_value, filename_out)
+    
+
+main()
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # TC1004B-202013
+
 Repositorio del bloque Implementación IoT
+
 Grupo 200
+
 Semestre 202013
